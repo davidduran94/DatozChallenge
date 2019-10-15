266c1219886358d73d1a55b8ca51bddbb3f6a051
David Duran <z056991@santander.com.mx>
2019-10-15 17:23:05 -0500

    Actions definitions

--- ecommerce-frontend/src/actions/index.js
+++ ecommerce-frontend/src/actions/index.js
@@ -3,31 +3,16 @@ export const setFavorite = payload => ({
     payload,
   });
   
-  export const loginRequest = payload => ({
-    type: 'LOGIN_REQUEST',
-    payload,
-  });
-  
-  export const logoutRequest = payload => ({
-    type: 'LOGOUT_REQUEST',
-    payload,
-  });
-  
-  export const registerRequest = payload => ({
-    type: 'REGISTER_REQUEST',
-    payload,
-  });
-  
-  export const deteleFavorite = payload => ({
+export const deteleFavorite = payload => ({
     type: 'DELETE_FAVORITE',
     payload,
   });
   
-  export const getItemsSource = payload => ({
+export const getItemsSource = payload => ({
     type: 'GET_ITEMS_SOURCE',
     payload,
   });
   
-  
+
   
   export { setFavorite as default }
