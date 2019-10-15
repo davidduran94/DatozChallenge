ba8661e80b6662288002885a8796df5e0e13ff87
David Duran <z056991@santander.com.mx>
2019-10-15 12:20:52 -0500

    congiguration an packages

---
+++ ecommerce-frontend/src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/home.scss'
+
+
+const Home = () => (
+    <div>
+        hoello
+    </div>
+)
+
+export default Home;
---
+++ ecommerce-frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home'
+
+
+ReactDOM.render(
+    <Home></Home>, 
+    document.getElementById("app")
+);
