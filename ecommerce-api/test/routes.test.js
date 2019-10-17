0168e1a5bfabacbc1a918369ba860e2bd3f8a7db
David <davidduran94@gmail.com>
2019-10-16 22:16:50 -0500

    Testind endpoints

---
+++ ecommerce-api/test/routes.test.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const proxyquire = require('proxyquire');
+
+    //filteredProducts,
+const { mockProds, ProductsServiceMock } = require('../utils/mocks/products');
+const testServer = require('../utils/testServer');
+
+describe('routes - products', function() {
+  const route = proxyquire('../routes/ecommerce', {
+    '../services/products': ProductsServiceMock
+  });
+
+  const request = testServer(route);
+  describe('GET products', function() {
+    
+    // test de generacion de datos de ML
+    it('should respond with status 200', function(done) {
+      request.get('/products/generate/ML').expect(200, done);
+    });
+
+    // test de generacion de datos de Kichink
+    it('should respond with status 200', function(done) {
+        request.get('/products/generate/Kichink').expect(200, done);
+      });
+    
+    /// test listado de productos Kichink
+    it('should respond with the list products', function(done) {
+      request.get('/products/Kichink').end((err, res) => {
+        assert.equal(res.body, {
+          data: mockProds.filter(item => item.name == 'Kichink'),
+          message: 'success'
+        });
+
+        done();
+      });
+    });
+
+    /// test listado de productos ML
+    it('should respond with the list products', function(done) {
+        request.get('/products/ML').end((err, res) => {
+          assert.equal(res.body, {
+            data: mockProds.filter(item => item.name == 'ML'),
+            message: 'success'
+          });
+  
+          done();
+        });
+      });
+
+
+  });
+});
--- ecommerce-api/utils/mocks/products.js
+++ ecommerce-api/utils/mocks/products.js
@@ -1,61 +1,104 @@
 const mockProds = [
-    {
-        id: 1457234,
-        name:'Kichink' ,
-        type:'',
-        url: 'https://www.kichink.com/',
-        cover: 'https://s3.amazonaws.com/img2-kichink/items_2096958_100728_0_20190207183922_b.jpg',
-        title: 'Chamarra negra',
-        description: 'Chamarra para hombre en paño de lana',
-        price: 450,
-
-    },
-    {
-      id: 214234,
-      name:'Kichink' ,
-      type:'',
-      url: 'https://www.kichink.com/',
-      cover: 'https://s3.amazonaws.com/img2-kichink/items_2344754_107662_3_20191008175646_b.jpg',
-      title: 'Reloj Clasic',
-      description: 'Reloj clasico en acabados de madera',
-      price: 4150,
-
-  },
   {
-    id: 149234,
-    name:'Kichink' ,
-    type:'',
-    url: 'https://www.kichink.com/',
-    cover: 'https://s3.amazonaws.com/img2-kichink/items_2345116_166438_2_20191009001334_b.jpg',
-    title: 'Tenis nike',
-    description: 'nike sport blanco c negro',
-    price: 1450,
-
+    "_id": "5da7c48949af5c62398e43f5",
+    "name": "ML",
+    "type": "",
+    "url": "https://www.mercadolibre.com.mx/",
+    "cover": "https://http2.mlstatic.com/D_Q_NP_750455-MLM31913531630_082019-AB.jpg",
+    "title": "Refrigerador Automático 251.19 L Mabe - Rma1025xmxe1",
+    "description": "Refrigerador Automático 251.19 L Mabe - Rma1025xmxe1",
+    "price": "5,999"
 },
 {
-  id: 142934,
-  name:'Kichink' ,
-  type:'',
-  url: 'https://www.kichink.com/',
-  cover: 'https://s3.amazonaws.com/img2-kichink/items_2343788_1434499_1_20191007185550_b.jpg',
-  title: 'T-Shirt',
-  description: 'Playera el triste moda',
-  price: 250,
-
-}
-,
+    "_id": "5da7c48949af5c62398e43f7",
+    "name": "ML",
+    "type": "",
+    "url": "https://www.mercadolibre.com.mx/",
+    "cover": "https://http2.mlstatic.com/D_Q_NP_851632-MLM31369505298_072019-AB.jpg",
+    "title": "Reloj Timex Tw2p62300 Con Perfume Nautica Envio Gratis",
+    "description": "Reloj Timex Tw2p62300 Con Perfume Nautica Envio Gratis",
+    "price": "1,499"
+},
 {
-  id: 13242934,
-  name:'AMZ' ,
-  type:'',
-  url: 'https://www.amazon.com/',
-  cover: 'https://images-na.ssl-images-amazon.com/images/I/310LszmgQvL._AA218_.jpg',
-  title: 'Suite',
-  description: 'Traje de Joker',
-  price: 2330,
+    "_id": "5da7c48949af5c62398e43f6",
+    "name": "ML",
+    "type": "",
+    "url": "https://www.mercadolibre.com.mx/",
+    "cover": "https://http2.mlstatic.com/D_Q_NP_997396-MLM31232374444_062019-AB.jpg",
+    "title": "Colección De Tés Zen Tea De 5 Sabores Con 150 Sobres",
+    "description": "Colección De Tés Zen Tea De 5 Sabores Con 150 Sobres",
+    "price": "175"
+},
+{
+    "_id": "5da7c48949af5c62398e43f9",
+    "name": "ML",
+    "type": "",
+    "url": "https://www.mercadolibre.com.mx/",
+    "cover": "https://http2.mlstatic.com/D_Q_NP_796425-MLM31523162603_072019-AB.jpg",
+    "title": "Caja Fuerte Electrónica Para Notebook - 22,5 Litros - Klatter",
+    "description": "Caja Fuerte Electrónica Para Notebook - 22,5 Litros - Klatter",
+    "price": "1,199"
+},
+{
+  "_id": "5da7c80852ea7463b1081c42",
+  "name": "Kichink",
+  "type": "",
+  "url": "https://www.kichink.com/",
+  "cover": "https://s3.amazonaws.com/kichink/items_2328048_419559_0_20190919162953_b.jpg",
+  "title": "Pin Noche",
+  "description": "Pin Noche",
+  "price": "$190.00 MXN"
+},
+{
+  "_id": "5da7c80852ea7463b1081c43",
+  "name": "Kichink",
+  "type": "",
+  "url": "https://www.kichink.com/",
+  "cover": "https://s3.amazonaws.com/kichink/items_2091528_377823_0_20190204133237_b.jpg",
+  "title": "DARK SOULS",
+  "description": "DARK SOULS",
+  "price": "$149.00 MXN"
+},
+{
+  "_id": "5da7c80852ea7463b1081c44",
+  "name": "Kichink",
+  "type": "",
+  "url": "https://www.kichink.com/",
+  "cover": "https://s3.amazonaws.com/kichink/items_2327819_419559_0_20190919134527_b.jpg",
+  "title": "Pin Mañana",
+  "description": "Pin Mañana",
+  "price": "$190.00 MXN"
+},
+{
+  "_id": "5da7c80852ea7463b1081c45",
+  "name": "Kichink",
+  "type": "",
+  "url": "https://www.kichink.com/",
+  "cover": "https://s3.amazonaws.com/kichink/items_2239322_53139_0_20190618091736_b.jpg",
+  "title": "La Gusana Ciega- Borregos En La Niebla 12\"",
+  "description": "La Gusana Ciega- Borregos En La Niebla 12\"",
+  "price": "$450.00 MXN"
+}
+];
+
+class ProductsServiceMock {
+  async getProducts(store) {
+    return Promise.resolve(mockProds.filter(item => item.name==store))
+  }
+
+  async createProduct() {
+    return Promise.resolve(mockProds[0]);
+  }
+
 }
 
-];
+
+function filteredProducts(tag){
+  return mockProds.filter(prod => prod.name.includes(tag))
+}
+
 module.exports = { 
-    mockProds 
+    mockProds ,
+    filteredProducts,
+    ProductsServiceMock
 };
